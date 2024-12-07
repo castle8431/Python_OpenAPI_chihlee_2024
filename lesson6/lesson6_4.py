73e50d02fca0015527fd7cb9980498956ec3578a
castle8431 <ggcastle8431@gmail.com>
2024-12-07 07:53:59 +0000

    m

--- lesson6/lesson6_4.py
+++ lesson6/lesson6_4.py
@@ -2,7 +2,18 @@ import streamlit as st
 import tools
 
 sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
-add_selectbox = st.sidebar.selectbox(
-    "請選擇站點名稱",
-    sitenames
-)
+# object notation
+#add_selectbox = st.sidebar.selectbox(
+#    "請選擇站點名稱",
+#    sitenames
+#)
+
+#"with" notation
+
+with st.sidebar:
+    add_selectbox = st.selectbox(
+        "請選擇站點名稱:",
+        sitenames
+    )
+
+    st.title(f"{add_selectbox}")
