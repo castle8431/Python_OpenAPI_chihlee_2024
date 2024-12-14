28bb857cea199d5854a3f107375f0184f5cf06db
castle8431 <ggcastle8431@gmail.com>
2024-12-14 06:18:43 +0000

    m

--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -1,17 +1,22 @@
 import requests 
 from requests import Response
+from requests import RequestException, HTTPError 
 from io import StringIO 
 from csv import DictReader 
 
 url = 'https://data.ntpc.gov.tw/api/datasets/75abd5e2-5454-4c59-973f-ba587ef94ccd/csv?page=0&size=1000'
 
-r:Response = requests.request('GET',url)
-if r.status_code == 200:
+try:
+    r:Response = requests.request('GET',url)
+    r.raise_for_status()
+except HTTPError as e:
+    print(e)
+except RequestException as e:
+    print(e)
+else:
     print("下載成功")
     file = StringIO(r.text)
     reader = DictReader(file)
     list_reader:list[dict] = list(reader)
     print(list_reader)
 
-else:
-    print("下載有問題")
---
+++ lesson7/tools.py
@@ -0,0 +1,23 @@
+import requests 
+from requests import Response
+from requests import RequestException, HTTPError 
+from io import StringIO 
+from csv import DictReader 
+
+def get_youbikes()->list[dict]:
+
+    url = 'https://data.ntpc.gov.tw/api/datasets/75abd5e2-5454-4c59-973f-ba587ef94ccd/csv?page=0&size=1000'
+
+    try:
+        r:Response = requests.request('GET',url)
+        r.raise_for_status()
+    except HTTPError as e:
+        raise Exception("伺服器有問題")
+    except RequestException as e:
+        raise Exception("連線有問題")
+    else:
+        print("下載成功")
+        file = StringIO(r.text)
+        reader = DictReader(file)
+        list_reader:list[dict] = list(reader)
+        return list_reader
