9502b6776e22d6d52c12525a4afefca6103dd4dc
castle8431 <ggcastle8431@gmail.com>
2024-12-14 08:16:25 +0000

    m

--- lesson7/tools/taipei.py
+++ lesson7/tools/taipei.py
@@ -1,12 +1,14 @@
 import requests 
 from requests import Response
-from requests import RequestException, HTTPError 
+from requests import RequestException, HTTPError
 from io import StringIO 
 from csv import DictReader 
+import streamlit as st
 
+@st.cache_data #暫存資料 沒有改變的資料不用重複rerun 增加效能
 def get_youbikes()->list[dict]:
 
-    url = 'https://data.ntpc.gov.tw/api/datasets/75abd5e2-5454-4c59-973f-ba587ef94ccd/csv?page=0&size=1000'
+    url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/csv?page=0&size=1000'
 
     try:
         r:Response = requests.request('GET',url)
